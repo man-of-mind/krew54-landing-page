c42bb58eb88bfc95ebb68d4a385216469a3a754c
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-11-29 23:32:46 +0100

    integrated waitlist api

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,5 +1,7 @@
-import { Modal, TextInput } from "@mantine/core";
+import { Alert, Modal, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import React from "react";
+import axios from "axios";
 
 const WaitlistModal = ({
   open,
@@ -8,6 +10,59 @@ const WaitlistModal = ({
   open: boolean;
   setOpened: (val: boolean) => void;
 }) => {
+  const [error, showError] = React.useState(false);
+  const [errorMsg, setErrorMsg] = React.useState("");
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [success, setSuccess] = React.useState(false)
+  const [successMsg, setSuccessMsg] = React.useState("")
+
+  const waitlistForm = useForm({
+    initialValues: {
+      email: "",
+      name: "",
+    },
+
+    validate: {
+      email: (value) =>
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+          ? null
+          : "Invalid email",
+    },
+  });
+
+  const handleSubmit = ({ email, name }: { email: string; name: string }) => {
+    setIsSubmitting(true);
+    showError(false)
+    setSuccess(false)
+    axios
+      .post(
+        "https://krew54.ew.r.appspot.com/waitlist",
+      { "email": email, "fullName": name }
+      )
+      .then((response) => {
+        console.log(response);
+        setIsSubmitting(false);
+        setSuccess(true)
+        setSuccessMsg(response.data.msg)
+      })
+      .catch((err) => {
+        try {
+          if (err?.response.status === 400) {
+            setErrorMsg(err.response.data.errors.msg);
+          } else if (err?.response.status === 422) {
+            setErrorMsg(err.response.data.error);
+          } else {
+            setErrorMsg("Hmmm, something went wrong, try again later.");
+          }
+        } catch (error) {
+          setErrorMsg("Hmmm, something went wrong, try again later.");
+        } finally {
+          showError(true);
+          setIsSubmitting(false);
+        }
+      });
+  };
+
   return (
     <Modal
       centered
@@ -17,19 +72,22 @@ const WaitlistModal = ({
       withCloseButton
       styles={() => ({
         title: {
-            fontSize: 30,
-            fontWeight: 'bolder',
-            color: '#1F6363'
+          fontSize: 30,
+          fontWeight: "bolder",
+          color: "#1F6363",
         },
         close: {
-            width: "40px",
-            height: "40px",
-            color: "#1F6363"
-        }
+          width: "40px",
+          height: "40px",
+          color: "#1F6363",
+        },
       })}
     >
       <div>
-        <form className="">
+        <form
+          onSubmit={waitlistForm.onSubmit((values) => handleSubmit(values))}
+          className="flex flex-col"
+        >
           <p className="text-center text-sm opacity-70 pb-3">
             Leveraging technology & social engagement to promote African`s
             financial inclusion.
@@ -42,6 +100,11 @@ const WaitlistModal = ({
             withAsterisk
             required
             size="md"
+            onFocusCapture={() => {
+              showError(false)
+              setSuccess(false)
+            }}
+            {...waitlistForm.getInputProps("email")}
             styles={() => ({
               input: {
                 height: "50px",
@@ -50,17 +113,22 @@ const WaitlistModal = ({
               },
               label: {
                 fontWeight: "bolder",
-                fontSize: "14px"
-              }
+                fontSize: "14px",
+              },
             })}
           />
           <TextInput
-            label="Name"
+            label="Full Name"
             placeholder="enter your name"
             id="name"
             withAsterisk
             required
             size="md"
+            onFocusCapture={() => {
+              showError(false)
+              setSuccess(false)
+            }}
+            {...waitlistForm.getInputProps("name")}
             styles={() => ({
               input: {
                 height: "50px",
@@ -69,13 +137,26 @@ const WaitlistModal = ({
               },
               label: {
                 fontWeight: "bolder",
-                fontSize: "14px"
-              }
+                fontSize: "14px",
+              },
             })}
           />
-          <div className="cursor-pointer font-bold px-4 py-3 mx-auto bg-green-100 w-fit rounded-lg mt-10 text-white-100">
-            Join Waitlist
-          </div>
+          {success && (
+            <Alert id="alert1" title="Sucess!" color="green">
+              {successMsg}
+            </Alert>
+          )}
+          {error && (
+            <Alert id="alert" title="Error!" color="red">
+              {errorMsg}
+            </Alert>
+          )}
+          <button
+            type="submit"
+            className="cursor-pointer self-center font-bold px-4 py-3 bg-green-100 w-fit rounded-lg mt-10 text-white-100"
+          >
+            {!isSubmitting ? "Join Waitlist" : "Loading..."}
+          </button>
         </form>
       </div>
     </Modal>
